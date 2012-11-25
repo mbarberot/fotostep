845de5907107aa9b19645478d6288d4240ff4a35
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 14:46:10 +0100

    Ajout du bean stateless vide : UserManager

---
+++ fotostepEJB/ejbModule/database/userManager/UserManager.java
@@ -0,0 +1,13 @@
+package database.userManager;
+
+import javax.ejb.Stateless;
+
+/**
+ * Bean de manipulation de l'entité User dans la base de données.
+ *
+ * @author Mathieu Barberot
+ */
+@Stateless
+public class UserManager implements UserManagerLocal
+{
+}
---
+++ fotostepEJB/ejbModule/database/userManager/UserManagerLocal.java
@@ -0,0 +1,13 @@
+package database.userManager;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale du bean UserManager
+ *
+ * @author Mathieu Barberot
+ */
+@Local
+public interface UserManagerLocal
+{
+}
