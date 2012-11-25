f8fd21777cb86830ed8b9f25e00faff8af88ba0e
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 16:02:18 +0100

    Ajout du bean stateful vide : UserSession

---
+++ fotostepEJB/ejbModule/business/UserSessionLocal.java
@@ -0,0 +1,13 @@
+package business;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale de manipulation du bean UserSession
+ * 
+ * @author Mathieu Barberot
+ */
+@Local
+public interface UserSessionLocal
+{
+}
