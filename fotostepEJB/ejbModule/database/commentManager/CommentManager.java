82a7b5a52460fb67cd5d7b2ca757d57db32d26db
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 14:54:12 +0100

    Ajout du bean stateless vide : CommentManager

---
+++ fotostepEJB/ejbModule/database/commentManager/CommentManager.java
@@ -0,0 +1,17 @@
+package database.commentManager;
+
+import javax.ejb.Stateless;
+
+/**
+ * Bean de manipulation de l'entité Comment dans la base de données.
+ *
+ * @author Mathieu Barberot
+ */
+@Stateless
+public class CommentManager implements CommentManagerLocal
+{
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
---
+++ fotostepEJB/ejbModule/database/commentManager/CommentManagerLocal.java
@@ -0,0 +1,14 @@
+package database.commentManager;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale du bean CommentManager
+ *
+ * @author Mathieu Barberot
+ */
+@Local
+public interface CommentManagerLocal
+{
+    
+}
