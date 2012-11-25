1bb153fe5cd5b8d0edaf19dd519bfd5a6c7cefc4
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 16:00:08 +0100

    Ajout du bean statefull vide : ImageImporter

---
+++ fotostepEJB/ejbModule/business/importbusiness/ImageImporterLocal.java
@@ -0,0 +1,13 @@
+package business.importbusiness;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale de manipulation du bean ImageImporter
+ * 
+ * @author Mathieu Barberot
+ */
+@Local
+public interface ImageImporterLocal
+{   
+}
