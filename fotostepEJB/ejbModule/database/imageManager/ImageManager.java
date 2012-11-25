8407940a592c6c774a282532621f20f80b0f2b0a
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 14:52:33 +0100

    Ajout du bean stateless vide : ImageManager

---
+++ fotostepEJB/ejbModule/database/imageManager/ImageManager.java
@@ -0,0 +1,13 @@
+package database.imageManager;
+
+import javax.ejb.Stateless;
+
+/**
+ * Bean de manipulation de l'entité Image dans la base de données.
+ *
+ * @author Mathieu Barberot
+ */
+@Stateless
+public class ImageManager implements ImageManagerLocal
+{
+}
---
+++ fotostepEJB/ejbModule/database/imageManager/ImageManagerLocal.java
@@ -0,0 +1,14 @@
+package database.imageManager;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale du bean ImageManager
+ *
+ * @author Mathieu Barberot
+ */
+@Local
+public interface ImageManagerLocal
+{
+    
+}
