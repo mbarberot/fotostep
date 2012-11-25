dac13a4b66ce75d4d043e490ffe11c35d5b3d1f8
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-11-25 14:47:39 +0100

    Ajout du bean stateless vide : AlbumManager

---
+++ fotostepEJB/ejbModule/database/albumManager/AlbumManagerLocal.java
@@ -0,0 +1,14 @@
+package database.albumManager;
+
+import javax.ejb.Local;
+
+/**
+ * Interface locale du bean AlbumManager
+ *
+ * @author Mathieu Barberot
+ */
+@Local
+public interface AlbumManagerLocal
+{
+    
+}
