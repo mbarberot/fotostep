4265c40bb8290c8c5591d794c15647ece1d06580
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-12-06 18:22:07 +0100

    Bean AlbumManager : interface locale terminée et première méthode implémentée, En cours de test.

--- fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManager.java
+++ fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManager.java
@@ -1,6 +1,16 @@
 package business.model.databaseManager.albumManager;
 
+import business.model.database.Album;
+import business.model.database.Image;
+import business.model.database.User;
+import business.util.Permission;
+import business.util.exceptions.AlbumNotFoundException;
+import business.util.exceptions.ImageNotFoundException;
+import business.util.exceptions.UserNotFoundException;
+import java.util.Date;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Bean de manipulation de l'entité Album dans la base de données.
@@ -10,4 +20,37 @@ import javax.ejb.Stateless;
 @Stateless
 public class AlbumManager implements AlbumManagerLocal
 {
+    @PersistenceContext
+    EntityManager em;
+
+    public void createAlbum(User user, String name, String description, Permission permission) throws UserNotFoundException
+    {
+        Album album = new Album();
+        album.setIdUser(user);
+        album.setName(name);
+        album.setDescription(description);
+        album.setPerm(permission.getPermission());
+        em.persist(em);
+    }
+
+    public void deleteAlbum(Album album) throws AlbumNotFoundException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void updateAlbum(Album album, String name, String description, Permission permission, Image image) throws AlbumNotFoundException, ImageNotFoundException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void likeAlbum(Album album, User user) throws AlbumNotFoundException, UserNotFoundException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void commentAlbum(Album album, User user, String title, Date date, String body) throws AlbumNotFoundException, UserNotFoundException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 }
--- fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManagerLocal.java
+++ fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManagerLocal.java
@@ -1,5 +1,13 @@
 package business.model.databaseManager.albumManager;
 
+import business.model.database.Album;
+import business.model.database.Image;
+import business.model.database.User;
+import business.util.Permission;
+import business.util.exceptions.AlbumNotFoundException;
+import business.util.exceptions.ImageNotFoundException;
+import business.util.exceptions.UserNotFoundException;
+import java.util.Date;
 import javax.ejb.Local;
 
 /**
@@ -11,4 +19,13 @@ import javax.ejb.Local;
 public interface AlbumManagerLocal
 {
     
+    public void createAlbum(User user, String name, String description, Permission permission) throws UserNotFoundException;
+
+    public void deleteAlbum(Album album) throws AlbumNotFoundException;
+
+    public void updateAlbum(Album album, String name, String description, Permission permission, Image image) throws AlbumNotFoundException, ImageNotFoundException;
+
+    public void likeAlbum(Album album, User user) throws AlbumNotFoundException, UserNotFoundException;
+
+    public void commentAlbum(Album album, User user, String title, Date date, String body) throws AlbumNotFoundException, UserNotFoundException;
 }
