7db44fcb4c5c114cfebba4d3011c5982bbfaf838
L5D <cholley.thibaut@gmail.com>
2012-12-14 10:47:18 +0100

    Fix auto-incr

--- fotostep/fotostepEJB/src/main/java/business/model/database/Userdata.java
+++ fotostep/fotostepEJB/src/main/java/business/model/database/Userdata.java
@@ -13,7 +13,6 @@ public class Userdata implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int idUser;
 
 	private long birthDate;
