4e6b46a7282a9b0bf3199f116e7bfc1a36eccef7
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com>
2012-12-19 17:03:17 +0100

    Adaptation de l'album manager avec la nouvelle base

--- fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManager.java
+++ fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManager.java
@@ -1,12 +1,13 @@
 package business.model.databaseManager.albumManager;
 
 import business.model.database.Album;
-import business.model.database.Image;
+import business.model.database.AuthorizationEnum;
+import business.model.database.Picture;
 import business.model.database.User;
-import business.util.Permission;
 import business.util.exceptions.AlbumNotFoundException;
-import business.util.exceptions.ImageNotFoundException;
+import business.util.exceptions.PictureNotFoundException;
 import business.util.exceptions.UserNotFoundException;
+import java.util.Date;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -22,22 +23,24 @@ public class AlbumManager implements AlbumManagerLocal
     @PersistenceContext
     EntityManager em;
 
-    public Album createAlbum(User user, String name, String description, Permission permission) throws UserNotFoundException{
+    public Album createAlbum(User user, String name, String description, AuthorizationEnum authorization) throws UserNotFoundException{
         Album album = new Album();
-        album.setIdUser(user);
-        album.setName(name);
+        
+        album.setAuthorization(authorization);
+        album.setDate(new Date());
         album.setDescription(description);
-        album.setPerm(permission.getPermission());
-        album.setDate(System.currentTimeMillis());
+        album.setName(name);
+        album.setUser(user);
+        
         em.persist(album);
         return album;
     }
 
-    public void deleteAlbum(Album album) throws AlbumNotFoundException{
-        em.remove(album);
+    public void deleteAlbum(Album album) throws AlbumNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public void updateAlbum(Album album, String name, String description, Permission permission, Image image) throws AlbumNotFoundException, ImageNotFoundException{
+    public void updateAlbum(Album album, String name, String description, AuthorizationEnum authorization, Picture picture) throws AlbumNotFoundException, PictureNotFoundException{
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
--- fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManagerLocal.java
+++ fotostep/fotostepEJB/src/main/java/business/model/databaseManager/albumManager/AlbumManagerLocal.java
@@ -1,11 +1,11 @@
 package business.model.databaseManager.albumManager;
 
 import business.model.database.Album;
-import business.model.database.Image;
+import business.model.database.AuthorizationEnum;
+import business.model.database.Picture;
 import business.model.database.User;
-import business.util.Permission;
 import business.util.exceptions.AlbumNotFoundException;
-import business.util.exceptions.ImageNotFoundException;
+import business.util.exceptions.PictureNotFoundException;
 import business.util.exceptions.UserNotFoundException;
 import javax.ejb.Local;
 
@@ -18,9 +18,9 @@ import javax.ejb.Local;
 public interface AlbumManagerLocal
 {
     
-    public Album createAlbum(User user, String name, String description, Permission permission) throws UserNotFoundException;
+    public Album createAlbum(User user, String name, String description, AuthorizationEnum authorization) throws UserNotFoundException;
 
     public void deleteAlbum(Album album) throws AlbumNotFoundException;
 
-    public void updateAlbum(Album album, String name, String description, Permission permission, Image image) throws AlbumNotFoundException, ImageNotFoundException;
+    public void updateAlbum(Album album, String name, String description, AuthorizationEnum authorization, Picture picture) throws AlbumNotFoundException, PictureNotFoundException;
 }
